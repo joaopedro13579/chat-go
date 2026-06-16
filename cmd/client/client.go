133774f3a5061c726e99f36821e74d657a7a5489
Marcelo Rodrigues <marques.marcelo.rodrigues@gmail.com>
2026-06-16 13:29:06 -0300

    add: client.go ask for server ip

--- cmd/client/client.go
+++ cmd/client/client.go
@@ -9,13 +9,28 @@ import (
 )
 
 func main() {
-	conn, err := net.Dial("tcp", "127.0.0.1:8080")
+	var serverAddr string
+	
+	if len(os.Args) > 1 {
+		serverAddr = os.Args[1] + ":8080"
+	} else {
+		fmt.Print("Enter server IP (or press Enter for localhost): ")
+		serverAddr, _ = bufio.NewReader(os.Stdin).ReadString('\n')
+		serverAddr = strings.TrimSpace(serverAddr)
+		if serverAddr == "" {
+			serverAddr = "127.0.0.1"
+		}
+		serverAddr = serverAddr + ":8080"
+	}
+	
+	conn, err := net.Dial("tcp", serverAddr)
 	if err != nil {
 		panic(err)
 	}
 	defer conn.Close()
 
-	fmt.Println("Connected to server")
+
+	fmt.Printf("Connected to server at %s\n", serverAddr)
 	fmt.Println("Enter your name: ")
 	myusername, _ := bufio.NewReader(os.Stdin).ReadString('\n')
 	conn.Write([]byte(myusername + "\n"))
